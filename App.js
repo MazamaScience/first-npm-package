3b6610b72822f6d462f3f70739ac026e40c4052f
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2023-01-06 15:42:54 -0800

    first push of test module

---
+++ App.js
@@ -0,0 +1,10 @@
+// Import myModule
+import Car from "./myModule.js";
+
+let mercedes = new Car();
+
+// Printing data
+console.log(mercedes);
+
+// Invoke static function
+mercedes.run();
---
+++ myModule.js
@@ -0,0 +1,9 @@
+// myModule module
+export default class Car {
+  run() {
+    console.log("Car running...");
+  }
+}
+
+// Export this module
+// module.exports = Car;
